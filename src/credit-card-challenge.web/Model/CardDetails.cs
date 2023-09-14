22c4a1135619c2144c4cbc6b7d815acd74a72cf6
Fredrick Amoako <fredrick.amoako@amalitech.com>
2023-09-14 16:04:16 +0000

    feat: add card and form components

---
+++ src/credit-card-challenge.web/Model/CardDetails.cs
@@ -0,0 +1,10 @@
+namespace CreditCardChallenge.web.Model;
+
+public class CardDetails
+{
+    public string CardNumber { get; set; }
+    public string CardName { get; set; }
+    public int Month { get; set; }
+    public int Year { get; set; }
+    public int Cvv { get; set; }
+}
